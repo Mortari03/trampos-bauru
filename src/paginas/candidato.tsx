9c7c0625c29f40440eb8495a7791852cebc67056
Mortari03 <133434029+Mortari03@users.noreply.github.com>
2024-07-13 13:41:58 -0300

    17,03

--- src/paginas/candidato.tsx
+++ src/paginas/candidato.tsx
@@ -4,9 +4,50 @@ import { Link } from "react-router-dom";
 function Candidato() {
     return (
 
-        <div>
-            <h1>Pagina de cadastro do Cadidato</h1>
-        </div>
+
+        <div className="tela_cad">
+
+            <div className="card_cad">
+
+                <h1>  O Seu Novo Proximo Emprego Está Aqui!</h1>
+
+                <h2> Crie sua conta para acessar e candidate-se à milhares de vagas. </h2>
+
+                <h3>Entre e comece a se candidatar</h3>
+
+                <div className="box_button">
+
+                    <button className="cad_conta_google">Google</button>
+
+                </div>
+
+                <h3>Ou Continue seu Cadastro Aqui!</h3>
+
+                <div className="box_input">
+                    <input type="Nome Completo" placeholder="Nome Completo" />
+                    <input type="Email" placeholder="Email" />
+                    <input type="password" placeholder="Senha" />
+                    <input type="Number" placeholder="CPF" />
+                    <select className="box_select" >
+                        <option value="" disabled selected>Área de Atuação</option>
+                        <option value="tecnologia">Tecnologia</option>
+                        <option value="saude">Saúde</option>
+                        <option value="educacao">Educação</option>
+                        <option value="administracao">Administração</option>
+                        <option value="engenharia">Engenharia</option>
+                    </select>
+                </div>
+
+                <div>
+                    <h3> <button className="accept_button"></button>  Li e concordo com as condições legais e a política de privacidade.</h3>
+                    <h3> <button className="accept_button"></button>  Autorizo o Trampos Facil a enviar comunicações comerciais sobre <br />
+                        produtos, serviços e eventos dos seus parceiros e colaboradores.</h3>
+                 
+                </div>
+            </div >
+
+        </div >
+
 
     )
 }
--- src/paginas/empresa.tsx
+++ src/paginas/empresa.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-
 import { Link } from "react-router-dom";
 
 function Empresa() {
     return (
 
-        <div>
-            <h1>Pagina de cadastro da Empresa</h1>
-        </div>
+        <div className="tela_cad">
+
+            <div className="card_cad">
+
+                <h1>  Faça Seu Cadastro e Publique suas Vagas Aqui!</h1>
+
+                <h2> Crie sua conta para acessar e candidate-se à milhares de vagas. </h2>
+
+            </div >
+
+        </div >
 
     )
 }
