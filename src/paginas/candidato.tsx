06abd5c52ec4dcdd5595fd8a833b3428b354b3d4
Mortari03 <133434029+Mortari03@users.noreply.github.com>
2024-07-17 23:07:02 -0300

    13.07

--- src/paginas/candidato.tsx
+++ src/paginas/candidato.tsx
@@ -3,32 +3,26 @@ import { Link } from "react-router-dom";
 
 function Candidato() {
     return (
-
-
         <div className="tela_cad">
-
             <div className="card_cad">
-
-                <h1>  O Seu Novo Proximo Emprego Está Aqui!</h1>
-
-                <h2> Crie sua conta para acessar e candidate-se à milhares de vagas. </h2>
-
-                <h3>Entre e comece a se candidatar</h3>
-
-                <div className="box_button">
-
-                    <button className="cad_conta_google">Google</button>
-
+                <div>
+                    <Link to="/" className="Botao-Voltar_cand">
+                        <img className="Voltar_Cand" src="/img/Voltar.png" alt="Voltar" />
+                    </Link>
                 </div>
-
+                <h1>O Seu Novo Proximo Emprego Está Aqui!</h1>
+                <h2>Crie sua conta para acessar e candidate-se à milhares de vagas.</h2>
+                <h3>Entre e comece a se candidatar</h3>
+                <div className="box_button">
+                    <button className="cad_conta_google">Google</button>
+                </div>
                 <h3>Ou Continue seu Cadastro Aqui!</h3>
-
                 <div className="box_input">
-                    <input type="Nome Completo" placeholder="Nome Completo" />
-                    <input type="Email" placeholder="Email" />
+                    <input type="text" placeholder="Nome Completo" />
+                    <input type="email" placeholder="Email" />
                     <input type="password" placeholder="Senha" />
-                    <input type="Number" placeholder="CPF" />
-                    <select className="box_select" >
+                    <input type="text" placeholder="CPF" />
+                    <select className="box_select">
                         <option value="" disabled selected>Área de Atuação</option>
                         <option value="tecnologia">Tecnologia</option>
                         <option value="saude">Saúde</option>
@@ -37,19 +31,22 @@ function Candidato() {
                         <option value="engenharia">Engenharia</option>
                     </select>
                 </div>
-
                 <div>
-                    <h3> <button className="accept_button"></button>  Li e concordo com as condições legais e a política de privacidade.</h3>
-                    <h3> <button className="accept_button"></button>  Autorizo o Trampos Facil a enviar comunicações comerciais sobre <br />
+                    <h3><button className="accept_button"></button> Li e concordo com as condições legais e a política de privacidade.</h3>
+                    <h3><button className="accept_button"></button> Autorizo o Trampos Facil a enviar comunicações comerciais sobre <br />
                         produtos, serviços e eventos dos seus parceiros e colaboradores.</h3>
-                 
                 </div>
-            </div >
+                <div>
+                    <input className="Button_Cad" type="button" value="Cadastre-se" />
+                </div>
 
-        </div >
+                <div className="imagem_cad">
+                    <img  className=" img_ilus "  src="/img/imgint.png" alt="ilustração" />
+                </div>
 
-
-    )
+            </div>
+        </div>
+    );
 }
 
 export default Candidato;
--- src/paginas/empresa.tsx
+++ src/paginas/empresa.tsx
@@ -8,14 +8,44 @@ function Empresa() {
 
             <div className="card_cad">
 
-                <h1>  Faça Seu Cadastro e Publique suas Vagas Aqui!</h1>
+            <div>
+                    <Link to="/" className="Botao-Voltar_cand">
+                        <img className="Voltar_Cand" src="/img/Voltar.png" alt="Voltar" />
+                    </Link>
+                </div>
 
-                <h2> Crie sua conta para acessar e candidate-se à milhares de vagas. </h2>
+                <h1> Publique Suas Vagas de Emprego Aqui!</h1>
+
+                <h2> Crie sua conta para acessar e crie sua Vagas. </h2>
+
+                <br />
+                <br />
+
+                <h3>Comece Seu Cadastro Aqui!</h3>
+                <br />
+                <br />
+                <div className="box_input">
+                    <input type="Nome Empresa" placeholder="Nome Empresa" />
+                    <input type="Email" placeholder="Email" />
+                    <input type="password" placeholder="Senha" />
+                    <input type="cpf" placeholder="CNPJ" />
+                </div>
+
+                <div>
+                    <h3> <button className="accept_button"></button>  Li e concordo com as condições legais e a política de privacidade.</h3>
+                    <h3> <button className="accept_button"></button>  Autorizo o Trampos Facil a enviar comunicações comerciais sobre <br />
+                        produtos, serviços e eventos dos seus parceiros e colaboradores.</h3>
+
+                </div>
+
+                <div>
+
+                    <input className="Button_Cad" type="button" value="Cadastre-se" />
+
+                </div>
 
             </div >
-
-        </div >
-
+        </div>
     )
 }
 
