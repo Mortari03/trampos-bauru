823bdacc85cffaa7129d0e65f5d7c8b2a09260a0
Mortari03 <133434029+Mortari03@users.noreply.github.com>
2024-07-22 20:15:52 -0300

    teste

--- src/paginas/prelogin.tsx
+++ src/paginas/prelogin.tsx
@@ -1,44 +1,40 @@
-// Importes do React
 import React from "react";
-// Importes do Router-Dom
 import { Link } from "react-router-dom";
-// Importes de Icones
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faUsers } from "@fortawesome/free-solid-svg-icons";
-// Importes de CSS
 import "../estilo/prelogin.css";
 
 function PreLogin() {
   return (
-    <div className="Conteiner-Cadastro">
-      <div className="Tela-cadastro">
+    <div className="container-cadastro">
+      <div className="tela-cadastro">
         <div>
-          <Link to="/" className="Botao-Voltar">
-            <img className="Voltar" src="/img/Voltar.png" alt="Voltar" />
+          <Link to="/" className="botao-voltar">
+            <img className="voltar" src="/img/Voltar.png" alt="Voltar" />
           </Link>
         </div>
         <div className="imagem">
-          <img className="Logo-cadastro" src="/img/Logo.png" alt="Logo" />
+          <img className="logo-cadastro" src="/img/Logo.png" alt="Logo" />
         </div>
-        <h1 className="Titulo-Cadastro">
+        <h1 className="titulo-cadastro">
           Faça seu Login No Trampos{" "}
-          <span className="Palavra-Destaque">Fácil</span> <br />E Venha Fazer
+          <span className="palavra-destaque">Fácil</span> <br />E Venha Fazer
           parte da Nossa Equipe
         </h1>
-        <div className="Centro-Cadastro">
-          <Link to="/login_candidato" className="Botao-Cadastro">
+        <div className="centro-cadastro">
+          <Link to="/login_candidato" className="botao-cadastro">
             <FontAwesomeIcon icon={faUser} className="icone-cadastro" />
-            <h1 className="Candidato">CANDIDATO</h1>
+            <h1 className="candidato">Candidato</h1>
           </Link>
-          <Link to="/login_empresa" className="Botao-Cadastro">
+          <Link to="/login_empresa" className="botao-cadastro">
             <FontAwesomeIcon icon={faUsers} className="icone-cadastro" />
-            <h1 className="Empresas">EMPRESA</h1>
+            <h1 className="empresa">Empresa</h1>
           </Link>
         </div>
-        <div className="Titulo-Final">
+        <div className="titulo-final">
           <h1>
             Você é novo no Trampos{" "}
-            <span className="Palavra-Destaque">Fácil</span>
+            <span className="palavra-destaque">Fácil</span>
           </h1>
           <h2 className="classe-mudavel">
             Cadastre-se como <Link to="/cadastro_candidatos">Candidato</Link> ou{" "}
