89d5853782dc9cb77bd8b3cf04f2dca9fff6ed1f
Mortari03 <133434029+Mortari03@users.noreply.github.com>
2024-07-22 20:10:52 -0300

    att 20:10

--- src/paginas/login_candidato.tsx
+++ src/paginas/login_candidato.tsx
@@ -1,14 +1,29 @@
-//Importes do React
-//Importes do Router-Dom
-//Importes de Icones
-//Importes de CSS
+import React from "react";
+// Importes do Router-Dom
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faUsers } from "@fortawesome/free-solid-svg-icons";
+// Importes de CSS
 import "../estilo/login_candidato.css";
-//Importes de Paginas
+// Importes de Paginas
+import { Link } from "react-router-dom";
 
 function LoginCandidato() {
   return (
-    <div>
-      <h1>Login Candidato</h1>
+    <div className="Conteiner_Login_cand">
+      <div className="Box_Login_cand">
+        <h1 className="class_login">
+          <p>LOGIN</p>
+        </h1>
+        <input type="text" placeholder="EMAIL" />
+        <br />
+        <input type="password" placeholder="SENHA" />
+        <Link to="/esqueci_senha" className="esqueci_senha">
+        <h3>Esqueci a Senha</h3>
+        </Link>
+        <div className="botao_login">
+          <button>CONTINUAR</button>
+        </div>
+      </div>
     </div>
   );
 }
