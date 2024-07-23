23530161c982bad7c51e41841746ca7bea984de0
Mortari03 <133434029+Mortari03@users.noreply.github.com>
2024-07-22 23:34:03 -0300

    att final 22/07 23:33

--- src/paginas/login_candidato.tsx
+++ src/paginas/login_candidato.tsx
@@ -1,27 +1,42 @@
 import React from "react";
-// Importes do Router-Dom
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faUsers } from "@fortawesome/free-solid-svg-icons";
-// Importes de CSS
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 import "../estilo/login_candidato.css";
-// Importes de Paginas
 import { Link } from "react-router-dom";
 
 function LoginCandidato() {
   return (
     <div className="Conteiner_Login_cand">
       <div className="Box_Login_cand">
-        <h1 className="class_login">
-          <p>LOGIN</p>
-        </h1>
+        <div className="header-login">
+          <Link to="/prelogin" className="botao-voltar">
+            <img className="voltar" src="/img/Voltar.png" alt="Voltar" />
+          </Link>
+          <img className="logo-cadastro" src="/img/Logo.png" alt="Logo" />
+        </div>
+        <FontAwesomeIcon icon={faUser} className="icone_cand"></FontAwesomeIcon>
+        <br />
+        <button>GOOGLE</button>
+        <br />
+        <h3 className="ponto_de_email">Ou Insira Seus <Link to="/pagina-a-linkar" className="email-link">Email</Link></h3>
+        <br />
         <input type="text" placeholder="EMAIL" />
         <br />
         <input type="password" placeholder="SENHA" />
-        <Link to="/esqueci_senha" className="esqueci_senha">
-        <h3>Esqueci a Senha</h3>
-        </Link>
+        <br />
+        <div className="link-esqueci-senha">
+          <Link to="/esqueci_senha" className="esqueci_senha">
+            <h3>Esqueci a Senha</h3>
+          </Link>
+        </div>
+        <br />
         <div className="botao_login">
           <button>CONTINUAR</button>
+        </div>
+        <div>
+          <br />
+          <h2>Voce é novo no <span className="texto_final_cand">TRAMPOS FÁCIL</span> ? </h2>
+          <br />
         </div>
       </div>
     </div>
--- src/paginas/login_empresa.tsx
+++ src/paginas/login_empresa.tsx
@@ -1,14 +1,43 @@
-//Importes do React
-//Importes do Router-Dom
-//Importes de Icones
-//Importes de CSS
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 import "../estilo/login_empresa.css";
-//Importes de Paginas
 
 function LoginEmpresa() {
   return (
-    <div>
-      <h1>Login Empresa</h1>
+    <div className="Conteiner_Login_Empre">
+      <div className="Box_Login_Empre">
+        <div className="header-login">
+          <Link to="/prelogin" className="botao-voltar">
+            <img className="voltar" src="/img/Voltar.png" alt="Voltar" />
+          </Link>
+          <img className="logo-cadastro" src="/img/Logo.png" alt="Logo" />
+        </div>
+        <FontAwesomeIcon icon={faUsers} className="icone-cadastro" />
+        <br />
+        <button>GOOGLE</button>
+        <br />
+        <h3 className="ponto_de_email">Ou Insira Seu <Link to="/pagina-a-linkar" className="email-link">Email</Link></h3>
+        <br />
+        <input type="text" placeholder="EMAIL" />
+        <br />
+        <input type="password" placeholder="SENHA" />
+        <br />
+        <div className="link-esqueci-senha">
+          <Link to="/esqueci_senha" className="esqueci_senha">
+            <h3>Esqueci a Senha</h3>
+          </Link>
+        </div>
+        <br />
+        <div className="botao_login">
+          <button>CONTINUAR</button>
+        </div>
+        <br />
+        <div>
+          <h2>Você é novo no <span className="texto_final_empr">TRAMPOS FÁCIL</span>?</h2>
+        </div>
+      </div>
     </div>
   );
 }
