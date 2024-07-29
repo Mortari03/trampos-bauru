bca0fbd3eff0fa11360006f7486a56b04d680cd4
VitorAluisio <vitorkingf@gmail.com>
2024-07-29 10:45:14 -0300

    Merge branch 'master' of https://github.com/Mortari03/trampos-bauru

--- src/paginas/candidato_perfil.tsx
+++ src/paginas/candidato_perfil.tsx
@@ -9,7 +9,56 @@ import "../estilo/candidato_perfil.css";
 function CandidatoPerfil() {
   return (
     <div>
-      <h1>Perfil Candidato</h1>
+      <div className="Container">
+        <div className="BordaEsquerda">
+          <h3>Perfil</h3>
+          <h3>Minhas vagas</h3>
+          <h3>Entrevistas agendadas</h3>
+          <h3>Configurações</h3>
+          <h3>Sair</h3>
+        </div>
+        <div className="AreaBio">
+          <h2>Nome Completo </h2>
+        </div>
+  
+      </div>
+
+      <div> <h2>Informações de Contato</h2>
+      </div>
+
+      <div>
+        <h2>Currículo</h2>
+      </div>
+
+      <div>
+        <h2> Último Emprego </h2>
+      </div>
+
+
+      <div>
+      <h2> Educação </h2>      
+       </div>
+       
+       <div>
+       <h2> Habilidades </h2>
+       </div>
+       
+       <div>
+       <h2> Certificações </h2>
+       </div>
+
+       <div>
+       <h2> Cursos </h2>
+       </div>
+      
+      <div>
+      <h2> Áreas de Interesse </h2>
+      </div>
+
+
+
+
+
       < Rodape />
     </div>
   );
