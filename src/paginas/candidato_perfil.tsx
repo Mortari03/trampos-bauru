8877bc5ab140b5044505ce749f03709dc4c45467
VitorAluisio <vitorkingf@gmail.com>
2024-07-29 13:29:14 -0300

    Página Perfil (incompleto)

--- src/paginas/candidato_perfil.tsx
+++ src/paginas/candidato_perfil.tsx
@@ -8,60 +8,54 @@ import "../estilo/candidato_perfil.css";
 
 function CandidatoPerfil() {
   return (
-    <div>
-      <div className="Container">
-        <div className="BordaEsquerda">
-          <h3>Perfil</h3>
-          <h3>Minhas vagas</h3>
-          <h3>Entrevistas agendadas</h3>
-          <h3>Configurações</h3>
-          <h3>Sair</h3>
-        </div>
-        <div className="AreaBio">
-          <h2>Nome Completo </h2>
-        </div>
-  
-      </div>
-
-      <div> <h2>Informações de Contato</h2>
-      </div>
-
-      <div>
-        <h2>Currículo</h2>
-      </div>
-
-      <div>
-        <h2> Último Emprego </h2>
-      </div>
-
-
-      <div>
-      <h2> Educação </h2>      
-       </div>
-       
-       <div>
-       <h2> Habilidades </h2>
-       </div>
-       
-       <div>
-       <h2> Certificações </h2>
-       </div>
-
-       <div>
-       <h2> Cursos </h2>
-       </div>
-      
-      <div>
-      <h2> Áreas de Interesse </h2>
-      </div>
-
-
-
-
-
-      < Rodape />
+    <div className="Container">
+    <div className="BordaEsquerda">
+      <br />
+      <h3>Perfil</h3>
+      <br />
+      <h3>Minhas vagas</h3>
+      <br />
+      <h3>Entrevistas agendadas</h3>
+      <br />
+      <h3>Configurações</h3>
+      <br />
+      <h3>Sair</h3>
     </div>
-  );
+    <div className="ContentWrapper">
+      <div className="MainContent">
+        <div className="AreaBio">
+          <h2>Nome Completo</h2>
+        </div>
+        <div className="Section">
+          <h2>Informações de Contato</h2>
+        </div>
+        <div className="Section">
+          <h2>Currículo</h2>
+        </div>
+        <div className="Section">
+          <h2>Último Emprego</h2>
+        </div>
+        <div className="Section">
+          <h2>Educação</h2>
+        </div>
+        <div className="Section">
+          <h2>Habilidades</h2>
+        </div>
+        <div className="Section">
+          <h2>Certificações</h2>
+        </div>
+        <div className="Section">
+          <h2>Cursos</h2>
+        </div>
+        <div className="Section">
+          <h2>Áreas de Interesse</h2>
+        </div>
+      </div>
+      <Rodape />
+    </div>
+  </div>
+);
 }
+
 
 export default CandidatoPerfil;
