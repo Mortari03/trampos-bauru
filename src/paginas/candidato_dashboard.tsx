fe11c36d3f91545272cf1912e33b3e3d7c28f02e
Lucas Lima <lucasmslima1@gmail.com>
2024-07-31 03:09:38 -0300

    31/07 - DashBoard Candidato
    DashBoard dos candidatos finalizada !

--- src/paginas/candidato_dashboard.tsx
+++ src/paginas/candidato_dashboard.tsx
@@ -1,18 +1,76 @@
 //Importes do React
-import React from "react";
+import React, { useState } from "react";
 //Importes do Router-Dom
+import { Link } from "react-router-dom";
 //Importes de Icones
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faClipboardList,
+  faCog,
+  faSignOutAlt,
+  faCalendarAlt,
+} from "@fortawesome/free-solid-svg-icons";
 //Importes de CSS
 import "../estilo/candidato_dashboard.css";
 //Importes de Paginas
 import CabecalhoCandidato from "../componentes/cabecalho_candidato";
 import Rodape from "../componentes/rodape";
+import CandidatoPerfil from "./candidato_perfil";
+import CandidatoVagas from "./candidato_minhas_vagas";
+import CandidatoEntrevistasAgendadas from "./candidato_entrevistas_agendadas";
+import CandidatoConfiguracao from "./candidato_configuracao";
 
 function CandidatoDashboard() {
+  const [secaoAtiva, setSecaoAtiva] = useState<string>("perfil");
+
+  const renderizarConteudo = () => {
+    switch (secaoAtiva) {
+      case "perfil":
+        return <CandidatoPerfil />;
+      case "minhasVagas":
+        return <CandidatoVagas />;
+      case "entrevistas":
+        return <CandidatoEntrevistasAgendadas />;
+      case "configuracoes":
+        return <CandidatoConfiguracao />;
+      default:
+        return <h2>Bem-vindo ao seu painel!</h2>;
+    }
+  };
+
   return (
     <div>
       <CabecalhoCandidato />
-      <div>candidato_dashboard</div>
+      <div className="dashboard-usuario">
+        <aside className="menu-lateral-usuario">
+          <div className="logo-container-usuario">
+            <img
+              src="/img/Logo.png"
+              alt="Logo Trampos Facil"
+              className="logo"
+            />
+          </div>
+          <nav className="menu-usuario">
+            <button onClick={() => setSecaoAtiva("perfil")}>
+              <FontAwesomeIcon icon={faUser} /> Meu Perfil
+            </button>
+            <button onClick={() => setSecaoAtiva("minhasVagas")}>
+              <FontAwesomeIcon icon={faClipboardList} /> Minhas Vagas
+            </button>
+            <button onClick={() => setSecaoAtiva("entrevistas")}>
+              <FontAwesomeIcon icon={faCalendarAlt} /> Entrevistas Agendadas
+            </button>
+            <button onClick={() => setSecaoAtiva("configuracoes")}>
+              <FontAwesomeIcon icon={faCog} /> Configurações
+            </button>
+            <Link to="/" className="botao-logout-usuario">
+              <FontAwesomeIcon icon={faSignOutAlt} /> Sair
+            </Link>
+          </nav>
+        </aside>
+        <main className="conteudo">{renderizarConteudo()}</main>
+      </div>
       <Rodape />
     </div>
   );
