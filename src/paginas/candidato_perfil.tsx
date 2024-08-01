eeca6914157ddb4b2f151ed90a51c908ef76bc6c
VitorAluisio <vitorkingf@gmail.com>
2024-08-01 10:37:27 -0300

    Atualizando falta somente designe

--- src/paginas/candidato_perfil.tsx
+++ src/paginas/candidato_perfil.tsx
@@ -26,7 +26,7 @@ function CandidatoPerfil() {
           </div>
 
 
-            <br />
+          <br />
 
           <div className="AnexarCurriculo">
             <h2>Currículo</h2>
@@ -35,27 +35,38 @@ function CandidatoPerfil() {
           <br />
 
           <div className="UltimoEmprego">
-            <h2>Último Emprego</h2>
+            <h2 className="UltimoEmpregoH2">Último Emprego</h2>
           </div>
+
+          <br />
 
           <div className="Educação">
-            <h2>Educação</h2>
+            <h2 className="EducaçãoH2">Educação</h2>
           </div>
+
+          <br />
 
           <div className="Habilidades">
-            <h2>Habilidades</h2>
+            <h2 className="HabilidadesH2">Habilidades</h2>
           </div>
+
+          <br />
 
           <div className="Certificações">
-            <h2>Certificações</h2>
+            <h2 className="CertificaçãoH2">Certificações</h2>
           </div>
+
+          <br />
 
           <div className="Cursos">
-            <h2>Cursos</h2>
+            <h2 className="CursosH2">Cursos</h2>
+
           </div>
 
-          <div className="Áreas de Interesse">
-            <h2>Áreas de Interesse</h2>
+          <br />
+
+          <div className="ÁreasInteresse">
+            <h2 className="InteressesH2">Áreas de Interesse</h2>
           </div>
         </div>
       </div>
