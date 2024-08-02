a85b4301f2c0e07b314f7ec5871b5e2492053458
VitorAluisio <vitorkingf@gmail.com>
2024-08-02 17:00:13 -0300

    Candidato Perfil (finalizado)

--- src/paginas/candidato_perfil.tsx
+++ src/paginas/candidato_perfil.tsx
@@ -12,7 +12,7 @@ function CandidatoPerfil() {
         <div className="MainContent">
 
           <div className="AreaBio">
-            <h2 className="NomeCompleto">Nome Completo</h2>
+            <input type="name" className="NomeCompleto" placeholder="Nome completo"/>
 
             <br />
             <textarea id="biografia" name="bio" placeholder="Escreva sua biografia aqui..."></textarea>
@@ -20,53 +20,66 @@ function CandidatoPerfil() {
 
           <div className="InfoContatos">
             <h2 className="InfoContatosH2">Informações de Contato</h2>
-            <p>Email</p>
-            <p>Telefone</p>
-            <p>Endereço</p>
-          </div>
 
+            <label htmlFor="email">Email:</label> <input type="email" id="email" />
+
+            <label htmlFor="phone">Telefone:</label> <input   type="tel"/>
+
+
+            <label htmlFor="address">Endereço:</label> <input
+              type="text"
+              id="address"
+            />
+          </div>
 
           <br />
 
           <div className="AnexarCurriculo">
             <h2>Currículo</h2>
-            <p>Anexar</p>
+            <label htmlFor="resume">Anexar currículo:</label>
+            <input type="file" id="resume" />
+
           </div>
           <br />
 
           <div className="UltimoEmprego">
             <h2 className="UltimoEmpregoH2">Último Emprego</h2>
+            <textarea id="BioEmprego" name="" placeholder="Descreva seu ultimo emprego..."></textarea>
           </div>
 
           <br />
 
           <div className="Educação">
             <h2 className="EducaçãoH2">Educação</h2>
+            <textarea name="BioEducação" id="" placeholder="Qual seu nível academico?"></textarea>
           </div>
 
           <br />
 
           <div className="Habilidades">
             <h2 className="HabilidadesH2">Habilidades</h2>
+            <textarea id="BioHabilidade" name="" placeholder="Quais são suas habilidades?"></textarea>
           </div>
 
           <br />
 
           <div className="Certificações">
             <h2 className="CertificaçãoH2">Certificações</h2>
+            <label htmlFor="resume">Anexar certificações</label> <input type="file" id="resume" />
           </div>
 
           <br />
 
           <div className="Cursos">
             <h2 className="CursosH2">Cursos</h2>
-
+            <textarea id="BioCursos" name="" placeholder="Tem algum curso? Quais são eles..."></textarea>
           </div>
 
           <br />
 
           <div className="ÁreasInteresse">
             <h2 className="InteressesH2">Áreas de Interesse</h2>
+            <textarea id="BioEmprego" name="" placeholder="Descreva seu ultimo emprego..."></textarea>
           </div>
         </div>
       </div>
