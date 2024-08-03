43f7d4952fa25ab1e63b2ff435ddeb97aa158200
Lucas Lima <lucasmslima1@gmail.com>
2024-08-03 19:30:59 -0300

    03/08 - Arrumando essa porra
    Padronização de body , h1 , h2 ,h3 , p , menu e botões !

--- src/paginas/quemsomos.tsx
+++ src/paginas/quemsomos.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../estilo/quemsomos.css';
+import "../estilo/quemsomos.css";
 import Cabecalho from "../componentes/cabecalho";
 import { faUser, faUsers } from "@fortawesome/free-solid-svg-icons";
 import Rodape from "../componentes/rodape";
@@ -11,61 +11,91 @@ function Quemsomos() {
       <Cabecalho />
 
       <div className="sobreo">
-        <h1>Sobre o <span className="tramposfacil">Trampos Fácil</span></h1>
+        <h1>
+          Sobre o <span className="tramposfacil">Trampos Fácil</span>
+        </h1>
       </div>
 
       <div className="sobrenoscontainer1">
         <div className="textobemvindos">
-          <h2>Bem-vindo ao Trampos Fácil! Somos uma equipe de estudantes do curso de Desenvolvimento Full Stack do Senac Bauru, <br />
-          unindo esforços para criar uma plataforma inovadora que facilita a busca por empregos na nossa cidade. <br />
-          Nosso objetivo é conectar candidatos a oportunidades de trabalho de forma eficiente e segura.</h2>
+          <h2>
+            Bem-vindo ao Trampos Fácil! Somos uma equipe de estudantes do curso
+            de Desenvolvimento Full Stack do Senac Bauru, <br />
+            unindo esforços para criar uma plataforma inovadora que facilita a
+            busca por empregos na nossa cidade. <br />
+            Nosso objetivo é conectar candidatos a oportunidades de trabalho de
+            forma eficiente e segura.
+          </h2>
         </div>
         <div className="imagemsobrenos"></div>
       </div>
 
       <div className="sobrenoscontainer2">
         <div className="nossamissao">
-          <h2>Facilitar o acesso a oportunidades de emprego para os cidadãos, <br />
-          promovendo uma experiência intuitiva e segura tanto para os candidatos quanto para as empresas.</h2>
+          <h2>
+            Facilitar o acesso a oportunidades de emprego para os cidadãos,{" "}
+            <br />
+            promovendo uma experiência intuitiva e segura tanto para os
+            candidatos quanto para as empresas.
+          </h2>
         </div>
         <div className="nossavisao">
-          <h2>Ser a principal plataforma de busca de empregos em Bauru, reconhecida pela sua eficácia e impacto positivo na comunidade.</h2>
+          <h2>
+            Ser a principal plataforma de busca de empregos em Bauru,
+            reconhecida pela sua eficácia e impacto positivo na comunidade.
+          </h2>
         </div>
       </div>
 
       <div className="sobrenostitulo2">
-        <h1>Nossos <span className="valores">Valores</span></h1>
+        <h1>
+          Nossos <span className="valores">Valores</span>
+        </h1>
       </div>
-      
+
       <div className="sobrenoscontainer3">
         <div className="inovacao">
-          <h4>Inovação: Buscamos constantemente melhorias e soluções criativas.</h4>
+          <h4>
+            Inovação: Buscamos constantemente melhorias e soluções criativas.
+          </h4>
         </div>
         <div className="transparencia">
-          <h4>Transparência: Valorizamos a honestidade e a clareza em todas as interações.</h4>
+          <h4>
+            Transparência: Valorizamos a honestidade e a clareza em todas as
+            interações.
+          </h4>
         </div>
         <div className="inclusao">
-          <h4>Inclusão: Acreditamos que todos merecem acesso a oportunidades justas.</h4>
+          <h4>
+            Inclusão: Acreditamos que todos merecem acesso a oportunidades
+            justas.
+          </h4>
         </div>
       </div>
 
       <div className="sobrenostitulo3">
-        <h1>Nossa <span className="equipe">Equipe</span></h1>
-        
+        <h1>
+          Nossa <span className="equipe">Equipe</span>
+        </h1>
       </div>
 
       <div className="sobrenostitulo4">
-        <h1>Impacto <span className="esperado">Esperado</span></h1>
+        <h1>
+          Impacto <span className="esperado">Esperado</span>
+        </h1>
       </div>
 
       <div className="textoimpacto">
-         <h2 className="textoimpacto1">O Trampo Fácil tem o potencial de revolucionar a forma como os bauruenses encontram emprego, <br />
-         contribuindo para a redução do desemprego local e promovendo o desenvolvimento econômico da região.</h2>
+        <h2 className="textoimpacto1">
+          O Trampo Fácil tem o potencial de revolucionar a forma como os
+          bauruenses encontram emprego, <br />
+          contribuindo para a redução do desemprego local e promovendo o
+          desenvolvimento econômico da região.
+        </h2>
       </div>
-      < Rodape />
+      <Rodape />
     </div>
   );
 }
 
 export default Quemsomos;
-
--- src/paginas/suporte.tsx
+++ src/paginas/suporte.tsx
@@ -1,15 +1,21 @@
-//Importes do React
+// Importes do React
 import React, { useState } from "react";
-//Importes do Router-Dom
-//Importes de Icones
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
-//Importes de CSS
-import "../estilo/suporte.css";
-//Importes de Paginas
+// Importes do Router-Dom
 import { Link } from "react-router-dom";
+// Importes de Icones
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronDown,
+  faChevronUp,
+  faUser,
+  faEnvelope,
+  faQuestionCircle,
+} from "@fortawesome/free-solid-svg-icons";
+// Importes de CSS
+import "../estilo/suporte.css";
+// Importes de Paginas
 import Rodape from "../componentes/rodape";
-//importes do Router-Dom
+import Cabecalho from "../componentes/cabecalho";
 
 function Suporte() {
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
@@ -18,152 +24,163 @@ function Suporte() {
     setActiveIndex(activeIndex === index ? null : index);
   };
 
-  const faqs = [
-    { question: "Como fazer meu cadastro?", answer: "Lorem ipsum sit amet." },
+  const faqsCandidatos = [
     {
-      question: "Como cadastrar o meu currículo?",
-      answer: "Lorem ipsum dolor amet.",
+      question: "Como posso me cadastrar?",
+      answer:
+        "Você pode se cadastrar clicando no botão 'Cadastrar' na página inicial e preenchendo o formulário.",
+      iconLeft: faUser,
     },
     {
-      question: "Posso fazer alterações em meu currículo após o cadastro?",
-      answer: "Lorem ipsum.",
+      question: "Como posso buscar vagas?",
+      answer:
+        "Utilize a barra de pesquisa e os filtros disponíveis para encontrar vagas que se encaixem no seu perfil.",
+      iconLeft: faQuestionCircle,
     },
     {
-      question: "Onde e como vou receber o retorno/feedback das empresas?",
-      answer: "Lorem, ipsum dolor.",
+      question: "Como posso atualizar meu perfil?",
+      answer:
+        "Acesse sua conta e vá até a seção 'Perfil' para editar suas informações e currículos.",
+      iconLeft: faUser,
     },
     {
-      question: "Como fazer meu cadastro?",
-      answer: "Lorem, ipsum dolor.",
+      question: "Como posso entrar em contato com o suporte?",
+      answer:
+        "Você pode entrar em contato conosco através do formulário nesta página.",
+      iconLeft: faEnvelope,
+    },
+  ];
+
+  const faqsEmpresas = [
+    {
+      question: "Como posso cadastrar uma vaga?",
+      answer: "Acesse sua conta e clique em 'Cadastrar Vaga'.",
+      iconLeft: faUser,
     },
     {
-      question: "Como fazer meu cadastro?",
-      answer: "Lorem ipsum dolor sitem.",
+      question: "Como posso gerenciar minhas vagas?",
+      answer: "Vá até a seção 'Minhas Vagas' em sua conta.",
+      iconLeft: faQuestionCircle,
     },
     {
-      question:
-        "Como altero ou retiro uma oferta de vaga do meu perfil empresarial?",
-      answer: "Lorem, ipsum dolor.",
+      question: "Como posso entrar em contato com o suporte?",
+      answer:
+        "Você pode entrar em contato conosco através do formulário nesta página.",
+      iconLeft: faEnvelope,
     },
     {
-      question:
-        "Terei algum custo adicional ao usar o Trampo Facil para Empresas?",
-      answer: "Lorem, ipsum dolor.",
+      question: "Terei algum custo adicional?",
+      answer:
+        "Não combramos nada pelos serviços utilizados em nossa plataforma !.",
+      iconLeft: faQuestionCircle,
     },
   ];
 
   return (
     <div>
-      <div className="Sup">
-        <h1 className="TagSup">Suporte</h1>
-        <br />
+      <Cabecalho />
+      <div className="suporte_container">
+        <div className="suporte_titulo_principal">
+          <h1>SUPORTE</h1>
+          <h3>
+            Estamos aqui para ajudar! Se você tiver alguma dúvida, entre em
+            contato conosco.
+          </h3>
+        </div>
 
-        <h3 className="TagPrincipal">
-          Estamos aqui para te ajudar! Se você tiver alguma dúvida, entre em
-          contato conosco
-        </h3>
-        <br />
-        <div className="ContainerSup">
-          <div className="DadosSup">
-            <div className="Nomebarra">
-              {" "}
-              <input type="Text" placeholder="Digite seu nome " />{" "}
-            </div>
-
-            <br />
-
-            <input className="BarraEmail" type="Email" placeholder="Email" />
-            <br />
-
+        <div className="suporte_formulario_container">
+          <div className="suporte_formulario">
             <input
+              type="text"
+              placeholder="Nome"
+              className="suporte_formulario_input"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="suporte_formulario_input"
+            />
+            <textarea
               placeholder="Digite sua mensagem"
-              className="BarraMensagem"
-            ></input>
-            <br />
-            <div className="botao_login">
-              <button>ENVIAR</button>
+              className="suporte_formulario_textarea"
+            ></textarea>
+            <button className="botões-principais">ENVIAR</button>
+          </div>
+          <div className="suporte_formulario_imagem" />
+        </div>
+
+        <div className="suporte_faq_container">
+          <h1>Perguntas Frequentes (FAQ)</h1>
+          <div className="suporte_faq_sections">
+            <div className="suporte_faq_section">
+              <img
+                src="/img/sup/ParaCandidatos.jpg"
+                alt="Para Candidatos"
+                className="suporte_faq_section_imagem"
+              />
+              <h2 className="suporte_faq_section_titulo">Para Candidatos</h2>
+              {faqsCandidatos.map((faq, index) => (
+                <div className="suporte_faq_item" key={index}>
+                  <div
+                    className="suporte_faq_item_pergunta"
+                    onClick={() => toggleDropdown(index)}
+                  >
+                    <FontAwesomeIcon
+                      icon={faq.iconLeft}
+                      className="suporte_faq_item_icon"
+                    />
+                    {faq.question}
+                    <FontAwesomeIcon
+                      icon={activeIndex === index ? faChevronUp : faChevronDown}
+                    />
+                  </div>
+                  {activeIndex === index && (
+                    <div className="suporte_faq_item_resposta">
+                      {faq.answer}
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+
+            <div className="suporte_faq_section">
+              <img
+                src="/img/sup/ParaEmpresas.jpg"
+                alt="Para Empresas"
+                className="suporte_faq_section_imagem"
+              />
+              <h2>Para Empresas</h2>
+              {faqsEmpresas.map((faq, index) => (
+                <div className="suporte_faq_item" key={index}>
+                  <div
+                    className="suporte_faq_item_pergunta"
+                    onClick={() =>
+                      toggleDropdown(index + faqsCandidatos.length)
+                    }
+                  >
+                    <FontAwesomeIcon
+                      icon={faq.iconLeft}
+                      className="suporte_faq_item_icon"
+                    />
+                    {faq.question}
+                    <FontAwesomeIcon
+                      icon={
+                        activeIndex === index + faqsCandidatos.length
+                          ? faChevronUp
+                          : faChevronDown
+                      }
+                    />
+                  </div>
+                  {activeIndex === index + faqsCandidatos.length && (
+                    <div className="suporte_faq_item_resposta">
+                      {faq.answer}
+                    </div>
+                  )}
+                </div>
+              ))}
             </div>
           </div>
-
-          <div className="AreaImagem">
-            <h2>Imagem a ser colocada</h2>
-          </div>
         </div>
-      </div>
-      <div className="Faq">
-        <br />
-        <br />
-
-        <h1 className="TagPrincipal">Perguntas frequentes FAQ</h1>
-        <br />
-        <br />
-        <div className="ContainerFaq">
-          <div>
-            <h2 className="ImgCandidatos">imagem candidatos</h2>
-          </div>
-          <div>
-            <h2 className="imgEmpresas">imagem empresa</h2>
-          </div>
-        </div>
-      </div>
-
-      <div className="DivCentro">
-        <div className="FAQContainer">
-          <div className="FAQColumn">
-            {faqs.slice(0, 4).map((faq, index) => (
-              <div className="FAQItem" key={index}>
-                <div
-                  className="FAQQuestion"
-                  onClick={() => toggleDropdown(index)}
-                >
-                  {faq.question}
-                  <FontAwesomeIcon
-                    icon={activeIndex === index ? faChevronUp : faChevronDown}
-                  />
-                </div>
-                {activeIndex === index && (
-                  <div className="FAQAnswer">{faq.answer}</div>
-                )}
-              </div>
-            ))}
-          </div>
-          <div className="FAQColumn">
-            {faqs.slice(4).map((faq, index) => (
-              <div className="FAQItem" key={index + 4}>
-                <div
-                  className="FAQQuestion"
-                  onClick={() => toggleDropdown(index + 4)}
-                >
-                  {faq.question}
-                  <FontAwesomeIcon
-                    icon={
-                      activeIndex === index + 4 ? faChevronUp : faChevronDown
-                    }
-                  />
-                </div>
-                {activeIndex === index + 4 && (
-                  <div className="FAQAnswer">{faq.answer}</div>
-                )}
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-
-      <div className="rodape">
-        <h4>
-          Ainda tem dúvidas?
-          <br />
-          Preencha nosso formulário do Google aqui ou <br />
-          nos mande um e-mail em suporte@trampofacil.com
-        </h4>
-        <br />
-      </div>
-
-      <div>
-        <Link to="/" className="Botao-Voltar">
-          <img className="Voltar" src="/img/Voltar.png" alt="Voltar" />
-        </Link>
       </div>
       <Rodape />
     </div>
