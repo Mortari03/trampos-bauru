b92461f94d81b6aa2bf7c4cbfc0227dab6486565
Mdellen39 <matheus.mrodrigues6@gmail.com>
2024-10-16 22:21:26 -0300

    Atualizado projeto
    Atualizado projeto

--- src/componentes/vagadetalhada/index.tsx
+++ src/componentes/vagadetalhada/index.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 import '../../estilo/vagadetalhada.css'
 
 // Adicione a nova prop 'nomeVaga' ao componente
-function VagaDetalhada({ nomeVaga, descricao, salario }: { nomeVaga: string; descricao: string; salario: String }) {
+function VagaDetalhada({ nomeVaga, descricao, salario, homeOffice, planoSaude, ajudaCusto }: { 
+  nomeVaga: string; descricao: string; salario: String, homeOffice: string; planoSaude:string; ajudaCusto:string; }) {
   return (
     <div className="vaga-detalhada">
     <h2 className="vaga-titulo">{nomeVaga}</h2>
@@ -11,6 +12,17 @@ function VagaDetalhada({ nomeVaga, descricao, salario }: { nomeVaga: string; des
     <div className="vaga-salario">
       <strong>Salário:</strong> <span>{salario}</span>
     </div>
+    <div className="vaga-home">
+      <strong>Homeo_ffice</strong> <span>{homeOffice}</span>
+    </div>
+    <div className="plano">
+      <strong>Plano</strong> <span>{planoSaude}</span>
+    </div>
+    <div className="vaga-home">
+      <strong>ajudaCusto</strong> <span>{ajudaCusto}</span>
+    </div>
+    
+
   </div>
 
   );
--- src/paginas/trampos.tsx
+++ src/paginas/trampos.tsx
@@ -9,34 +9,56 @@ type Vaga = {
   nomeVaga: string;
   descricao: string;
   Salario: string;
+  homeOffice?: string;
+  planoSaude?: string;
+  ajudaCusto?: string;
 };
 
 // Lista de vagas disponíveis
 const vagas: Vaga[] = [
-  { id: 1, nomeVaga: "Desenvolvedor Frontend", 
-           descricao: "Vaga para desenvolvedor frontend com experiência em React.",
-           Salario:'1.200,00'},
-  { id: 2, nomeVaga: "Desenvolvedor Backend", 
-           descricao: "Vaga para desenvolvedor backend com experiência em Node.js.",
-           Salario:'1.200,00' },
-  { id: 3, nomeVaga: "DevOps", 
-           descricao: "Vaga para especialista em DevOps e infraestrutura.",
-           Salario:'1.200,00' },
-  { id: 4, nomeVaga: "Analista de Sistemas", 
-           descricao: "Vaga para especialista em DevOps e infraestrutura.",
-           Salario:'1.200,00' },
-  { id: 5, nomeVaga: "Product Owner", 
-           descricao: "Vaga para especialista em Requisitos e Gestão de Backlog.",
-           Salario:'1.200,00' },
-  { id: 6, nomeVaga: "Product Manager", 
-           descricao: "Vaga para profissional empenhado em manter seu produto atual e moderno.",
-           Salario:'1.200,00' },
-  { id: 7, nomeVaga: "Head Tech", 
-           descricao: "Vaga para gestor de equipe de TI com ênfase em Pessoas.",
-           Salario:'1.200,00' },
-  { id: 8, nomeVaga: "Desenvolvedor Java",
-           descricao: "Vaga para especialista em desenvolvimento Java",
-           Salario:'1.200,00' },
+  {
+    id: 1, nomeVaga: "Desenvolvedor Frontend",
+    descricao: "Vaga para desenvolvedor frontend com experiência em React.",
+    Salario: '1.200,00',
+    homeOffice: 'true',
+    planoSaude: 'true',
+    ajudaCusto: 'true',
+  },
+  {
+    id: 2, nomeVaga: "Desenvolvedor Backend",
+    descricao: "Vaga para desenvolvedor backend com experiência em Node.js.",
+    Salario: '1.200,00'
+  },
+  {
+    id: 3, nomeVaga: "DevOps",
+    descricao: "Vaga para especialista em DevOps e infraestrutura.",
+    Salario: '1.200,00'
+  },
+  {
+    id: 4, nomeVaga: "Analista de Sistemas",
+    descricao: "Vaga para especialista em DevOps e infraestrutura.",
+    Salario: '1.200,00'
+  },
+  {
+    id: 5, nomeVaga: "Product Owner",
+    descricao: "Vaga para especialista em Requisitos e Gestão de Backlog.",
+    Salario: '1.200,00'
+  },
+  {
+    id: 6, nomeVaga: "Product Manager",
+    descricao: "Vaga para profissional empenhado em manter seu produto atual e moderno.",
+    Salario: '1.200,00'
+  },
+  {
+    id: 7, nomeVaga: "Head Tech",
+    descricao: "Vaga para gestor de equipe de TI com ênfase em Pessoas.",
+    Salario: '1.200,00'
+  },
+  {
+    id: 8, nomeVaga: "Desenvolvedor Java",
+    descricao: "Vaga para especialista em desenvolvimento Java",
+    Salario: '1.200,00'
+  },
 ];
 
 function Trampos() {
@@ -50,7 +72,7 @@ function Trampos() {
   return (
     <div>
       <Cabecalho />
-     {/* <div className="Conteiner">
+      {/* <div className="Conteiner">
          <div className="VagasResumo">
           {vagas.map((vaga) => (
             <button
@@ -76,31 +98,35 @@ function Trampos() {
         </div>
       </div>
     </div> */}
-    <div className="container">
-      <div className="vagas-resumo">
-        {vagas.map((vaga) => (
-          <div
-            key={vaga.id}
-            className="vaga-card"
-            onClick={() => handleVagaClick(vaga)}
-          >
-            <span className="vaga-nome">{vaga.nomeVaga}</span>
-          </div>
-        ))}
-      </div>
+      <div className="container">
+        <div className="vagas-resumo">
+          {vagas.map((vaga) => (
+            <div
+              key={vaga.id}
+              className="vaga-card"
+              onClick={() => handleVagaClick(vaga)}
+            >
+              <span className="vaga-nome">{vaga.nomeVaga}</span>
+            </div>
+          ))}
+        </div>
 
-      <div className="vaga-detalhe">
-        {vagaSelecionada ? (
-          <VagaDetalhada
-            nomeVaga={vagaSelecionada.nomeVaga}
-            descricao={vagaSelecionada.descricao}
-            salario={vagaSelecionada.Salario}
-          />
-        ) : (
-          <p>Selecione uma vaga para ver os detalhes.</p>
-        )}
+        <div className="vaga-detalhe">
+          {vagaSelecionada ? (
+            <VagaDetalhada
+              nomeVaga={vagaSelecionada.nomeVaga}
+              descricao={vagaSelecionada.descricao}
+              salario={vagaSelecionada.Salario}
+              homeOffice={vagaSelecionada.homeOffice ?? 'Não informado'}  // Verifica se homeOffice existe
+              planoSaude={vagaSelecionada.planoSaude ?? 'Não informado'}  // Verifica se planoSaude existe
+              ajudaCusto={vagaSelecionada.ajudaCusto ?? 'Não informado'}  // Verifica se ajudaCusto existeo
+
+            />
+          ) : (
+            <p>Selecione uma vaga para ver os detalhes.</p>
+          )}
+        </div>
       </div>
-    </div>
     </div>
   );
 }
